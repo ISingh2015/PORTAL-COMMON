0c9071bb58cff7a1aa78cb3e6b14c804505f0ff1
Inderjit SS <inderjitsanhotra@gmail.com>
2019-10-23 11:58:47 +0530

    Updated POM, Exception

--- src/main/java/com/inderjit/portal/exception/RecordNotFoundException.java
+++ src/main/java/com/inderjit/portal/exception/RecordNotFoundException.java
@@ -1,6 +1,10 @@
 package com.inderjit.portal.exception;
 
-public class RecordNotFoundException extends Exception{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RecordNotFoundException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 
